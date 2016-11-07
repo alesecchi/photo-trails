c443e8eff2d50f117ad507c399e91fed773f8000
ealesec <alessandro.secchi@ericsson.com>
2016-11-07 12:20:23 +0100

    Added Data access service for trail entity. Added unit test for this service and the required reference dependencies.

---
+++ PhotoTrailsWebServices/PhotoTrailsWebServices/DataAccessService/TrailDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PhotoTrailsWebServices.DataAccessService
+{
+    public class TrailDataAccess
+    {
+        private phototrailsEntities _context;
+
+        public TrailDataAccess(phototrailsEntities context)
+        {
+            _context = context;
+        }
+
+        public List<trail> GetAllTrails()
+        {
+            var query = from trail in _context.trail
+                        orderby trail.name
+                        select trail;
+            return query.ToList();
+        }
+
+        public async Task<List<trail>> GetAllTrailsAsync()
+        {
+            var query = from trail in _context.trail
+                        orderby trail.name
+                        select trail;
+            return await query.ToListAsync();
+        }
+    }
+}
---
+++ PhotoTrailsWebServices/PhotoTrailsWebServicesTest/DataAccessService/TrailDataAccessTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace PhotoTrailsWebServices.DataAccessService.Tests
+{
+    [TestClass()]
+    public class TrailDataAccessTests
+    {
+        [TestMethod()]
+        public void GetAllTrails_OrderByName()
+        {
+            var data = new List<trail>
+            {
+                new trail { id=1, name="AAA" },
+                new trail { id=1, name="ZZZ" },
+                new trail { id=1, name="BBB" }
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<trail>>();
+            mockSet.As<IQueryable<trail>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<trail>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<trail>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<trail>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<phototrailsEntities>();
+            mockContext.Setup(c => c.trail).Returns(mockSet.Object);
+
+            TrailDataAccess trailDataAccess = new TrailDataAccess(mockContext.Object);
+            var trails = trailDataAccess.GetAllTrails();
+
+            Assert.AreEqual(3, trails.Count);
+            Assert.AreEqual("AAA", trails[0].name);
+            Assert.AreEqual("BBB", trails[1].name);
+            Assert.AreEqual("ZZZ", trails[2].name);
+        }
+    }
+}
